b9269bf57b0b5e802827f4520abee0b823d81d56
de-110 <183182702+de-110@users.noreply.github.com>
2024-12-03 15:49:09 +0800

    Feature photo_processor.py
    -add: made a function to set the image path in the img_path line edit
    -add: open_file method to manage preview_img and set_img_path
    -add: handle_file method to manage photo id creation logic
    -add: preview images based on the users selected photo

--- photo_processor.py
+++ photo_processor.py
@@ -2,31 +2,75 @@ import sys
 import os
 
 from PyQt6.QtWidgets import QMainWindow, QFileDialog
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
+from PyQt6 import uic
 
 from docx.shared import Inches
 from docx import Document
 
 desktop = os.path.join(os.path.expanduser("~"))
 
-class PhotoID_Processor(QMainWindow):
+photo_processor_ui, classinfo = uic.loadUiType('photo_processor.ui')
+
+class PhotoID_Processor(QMainWindow, photo_processor_ui):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
+    
+        self.document = Document()
 
-        document = Document()
-        self.open_file()
+        self.selected_file = None
+
+        self.set_img_path()
+
+        self.id_type.addItems(['','2x2', '1x1'])
+
+        self.browse_button.clicked.connect(self.open_file)
+        self.process_button.clicked.connect(self.handle_file)
+    
+    def set_img_path(self):
+        if not self.selected_file:
+            self.img_path.setText(desktop)
+        else:
+            self.img_path.setText(self.selected_file)
 
     def open_file(self):
-        print("open file")
-        selected_file = self.open_dialog('open_file')
-        print(selected_file)
+        self.selected_file = self.open_dialog('open_file')
+        self.preview_image()
+        self.set_img_path()
     
-    def process_file(self):
-        print("process file")
+    def handle_file(self):
+        id_type = self.id_type.currentText()
+        num_of_photos = self.num_of_photos.text()
+
+        if not self.selected_file or not num_of_photos:
+            print('configuration are empty cannot continue!!')
+
+        else:
+            match id_type:
+                case '2x2':                    
+                    self.create_photoID(2, num_of_photos, self.selected_file)
+
+                case '1x1':
+                    print('generate 1x1 images')
+                
+                case '':
+                    print('please select and id type!!')
+
+    def create_photoID(self, size, num_of_photos, selected_file):
+        for photos in num_of_photos:
+            self.document.add_picture(selected_file, width=Inches(size), height=Inches(size))
+        self.document.save('test.docx')
 
     def save_file(self):
         print("save file")
-        selected_file = self.open_dialog('save_file')
-        print(selected_file)
+        self.selected_file = self.open_dialog('save_file')
+    
+    def preview_image(self):
+        pixmap = QPixmap(self.selected_file)
+        scaled_pixmap = pixmap.scaled(self.preview_img.size(), Qt.AspectRatioMode.KeepAspectRatio)
+        self.preview_img.setPixmap(scaled_pixmap)
         
     def open_dialog(self, mode):
         dialog = QFileDialog(self)
