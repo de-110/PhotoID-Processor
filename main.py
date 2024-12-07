ed2a73ad04cafa4d8123d7ee94ee1d8b3ee7cf17
de-110 <183182702+de-110@users.noreply.github.com>
2024-12-07 10:56:42 +0800

    refactor: setup a window icon,  window title and set a AppUserModelID for taskbar grouping

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import ctypes
 import sys
 import os
 
 from PyQt6.QtWidgets import QApplication, QStyleFactory
+from PyQt6.QtGui import QIcon
+
 from photo_processor import PhotoID_Processor
+
+try:
+    myappid = 'com.d-110.PhotoID-Processor.v0.1.0'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+except ImportError:
+    pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("main-icon.png"))
     app.setApplicationName("PhotoID Processor")
     app.setStyle(QStyleFactory.create('Fusion'))
     
     window = PhotoID_Processor()
+    window.setWindowTitle("PhotoID Processor")
     window.show()
     sys.exit(app.exec())
 
